281a316b45636dfc139401ce8dbf4d8ae1fbde08
Daigo Hirooka <daigo.hirooka@gmail.com>
2020-08-16 02:59:16 +0900

    add AdaCos layer #1

--- tf_simple_metric_learning/layers.py
+++ tf_simple_metric_learning/layers.py
@@ -15,7 +15,8 @@ class CosineSimilarity(layers.Layer):
     def build(self, input_shape):
         input_dim = input_shape[-1]
         self.W = self.add_weight(shape=(input_dim, self.num_classes),
-                                 initializer='random_normal')
+                                 initializer='random_normal',
+                                 trainable=True)
 
     def call(self, inputs):
         x = tf.nn.l2_normalize(inputs, axis=-1) # (batch_size, ndim)
@@ -53,3 +54,36 @@ class ArcFaceLoss(losses.Loss):
         
     def call(self, y_true, y_pred):
         return arcface_loss(y_true, y_pred, self.margin, self.scale)
+
+
+class AdaCos(layers.Layer):
+    def __init__(self, num_classes, **kwargs):
+        super().__init__(**kwargs)
+        self.num_classes = num_classes
+
+        self.scale = tf.Variable(tf.sqrt(2)*tf.math.log(num_classes - 1),
+                                 trainable=False)
+
+    def call(self, inputs, training):
+        # In inference, labels is ignored
+        cos, labels = inputs
+        labels = tf.cast(labels, dtype=cos.dtype)
+        
+        # Collect cosine values at only false labels
+        B = (1 - labels)*tf.exp(self.scale*cos)
+        B_avg = tf.reduce_mean(tf.reduce_sum(B, axis=-1), axis=0)
+
+        theta = tf.acos(tf.clip_by_value(cos, -1, 1))
+        # Collect cosine at true labels
+        theta_true = tf.reduce_sum(labels*theta, axis=-1)
+        # get median (=50-percentile)
+        theta_med = tfp.stats.percentile(theta_true, q=50)
+
+        scale = tf.math.log(B_avg) / tf.cos(tf.minimum(np.pi/4, theta_med))
+
+        if training:
+            self.scale.assign(scale)
+            
+        scale = tf.stop_gradient(scale)
+        logits = scale*cos
+        return logits
